8c9fbcdb413ee5fbadf8844279af7976e078a5d9
Andreivan <andreivan.santos@gmail.com>
2020-04-27 15:42:40 +0200

    First commit react basis

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    var user = {
+      name: "Anna",
+      hobbies: ["Sports", "Music", "Nefflix"]
+    }
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Header/>
+          </div>
+        </div>        
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Home name={"Andreivan"} age={32} user={user} height="1.95cm">
+              <p>Juca Bala!!!</p>
+            </Home>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
